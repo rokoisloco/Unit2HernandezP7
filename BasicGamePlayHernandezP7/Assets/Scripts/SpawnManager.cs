30d726f5efc8323ee7e5a45a3ed4db53a2c5d737
Byron Hernandez <739744@student.dusd.net>
2024-03-18 15:25:50 -0700

    2.4 Step 1
    In 2.4 Step 1

--- BasicGamePlayHernandezP7/Assets/Scripts/SpawnManager.cs
+++ BasicGamePlayHernandezP7/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
   
     
     // Start is called before the first frame update
@@ -17,6 +18,17 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if (Input.GetKeyDown(KeyCode.S))
+        {
+          SpawnRandomAnimal();
+        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
     }
 }
