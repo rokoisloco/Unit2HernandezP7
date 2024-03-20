4865d455af88ebb92b00cb1f363b3c070a0c82ef
Byron Hernandez <739744@student.dusd.net>
2024-03-20 15:24:23 -0700

    Bonusses
    Finished vertical player movement. Need to do 2 more bonusses.

--- BasicGamePlayHernandezP7/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayHernandezP7/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
     public GameObject projectilePrefab;
+
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +37,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if(transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.z, zMin);
+        }
+        if((transform.position.z > zMax))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
